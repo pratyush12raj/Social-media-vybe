a5c2284ffcdba3931271a1fb5e36fea496a6d3a3
Pratyush Raj <143428765+pratyush12raj@users.noreply.github.com>
2026-05-19 20:32:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
@@ -17,7 +19,7 @@ import Story from './pages/Story'
 import getAllStories from './hooks/getAllStories'
 import Messages from './pages/Messages'
 import MessageArea from './pages/MessageArea'
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 import { setOnlineUsers, setSocket } from './redux/socketSlice'
 import getFollowingList from './hooks/getFollowingList'
 import getPrevChatUsers from './hooks/getPrevChatUsers'
@@ -25,65 +27,138 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="https://social-media-vybe-backend-o48n.onrender.com"
+
+export const serverUrl = "https://social-media-vybe-backend-o48n.onrender.com"
+
 function App() {
-   getCurrentUser()
-   getSuggestedUsers()
-   getAllPost()
-   getAllLoops()
-   getAllStories()
-   getFollowingList()
-   getPrevChatUsers()
-   getAllNotifications()
-  const {userData,notificationData}=useSelector(state=>state.user)
-   
-    const {socket}=useSelector(state=>state.socket)
-    const dispatch=useDispatch()
- useEffect(()=>{
-  if(userData){
-    const socketIo=io(`${serverUrl}`,{
-      query:{
-        userId:userData._id
+
+  getCurrentUser()
+  getSuggestedUsers()
+  getAllPost()
+  getAllLoops()
+  getAllStories()
+  getFollowingList()
+  getPrevChatUsers()
+  getAllNotifications()
+
+  const { userData, notificationData } = useSelector(state => state.user)
+  const { socket } = useSelector(state => state.socket)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+    if (userData) {
+
+      const socketIo = io(serverUrl, {
+        query: {
+          userId: userData._id
+        },
+        withCredentials: true
+      })
+
+      dispatch(setSocket(socketIo))
+
+      socketIo.on("getOnlineUsers", (users) => {
+        dispatch(setOnlineUsers(users))
+        console.log(users)
+      })
+
+      return () => socketIo.close()
+
+    } else {
+
+      if (socket) {
+        socket.close()
+        dispatch(setSocket(null))
       }
-    })
-dispatch(setSocket(socketIo))
 
-
-socketIo.on('getOnlineUsers',(users)=>{
-  dispatch(setOnlineUsers(users))
-  console.log(users)
-})
-
-
-return ()=>socketIo.close()
-  }else{
-    if(socket){
-      socket.close()
-      dispatch(setSocket(null))
     }
-  }
- },[userData])
 
+  }, [userData])
 
-socket?.on("newNotification",(noti)=>{
-  dispatch(setNotificationData([...notificationData,noti]))
-})
+  useEffect(() => {
+
+    if (socket) {
+      socket.on("newNotification", (noti) => {
+        dispatch(setNotificationData([...notificationData, noti]))
+      })
+    }
+
+    return () => {
+      socket?.off("newNotification")
+    }
+
+  }, [socket, notificationData])
 
   return (
     <Routes>
-      <Route path='/signup' element={!userData?<SignUp/>:<Navigate to={"/"}/>}/>
-       <Route path='/signin' element={!userData?<SignIn/>:<Navigate to={"/"}/>}/>
-        <Route path='/' element={userData?<Home/>:<Navigate to={"/signin"}/>}/>
-       <Route path='/forgot-password' element={!userData?<ForgotPassword/>:<Navigate to={"/"}/>}/>
-      <Route path='/profile/:userName' element={userData?<Profile/>:<Navigate to={"/signin"}/>}/>
-      <Route path='/story/:userName' element={userData?<Story/>:<Navigate to={"/signin"}/>}/>
-       <Route path='/upload' element={userData?<Upload/>:<Navigate to={"/signin"}/>}/>
-        <Route path='/search' element={userData?<Search/>:<Navigate to={"/signin"}/>}/>
-          <Route path='/editprofile' element={userData?<EditProfile/>:<Navigate to={"/signin"}/>}/>
-            <Route path='/messages' element={userData?<Messages/>:<Navigate to={"/signin"}/>}/>
-            <Route path='/messageArea' element={userData?<MessageArea/>:<Navigate to={"/signin"}/>}/>
-             <Route path='/notifications' element={userData?<Notifications/>:<Navigate to={"/signin"}/>}/>
-          <Route path='/loops' element={userData?<Loops/>:<Navigate to={"/signin"}/>}/>
+
+      <Route
+        path='/signup'
+        element={!userData ? <SignUp /> : <Navigate to={"/"} />}
+      />
+
+      <Route
+        path='/signin'
+        element={!userData ? <SignIn /> : <Navigate to={"/"} />}
+      />
+
+      <Route
+        path='/'
+        element={userData ? <Home /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/forgot-password'
+        element={!userData ? <ForgotPassword /> : <Navigate to={"/"} />}
+      />
+
+      <Route
+        path='/profile/:userName'
+        element={userData ? <Profile /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/story/:userName'
+        element={userData ? <Story /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/upload'
+        element={userData ? <Upload /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/search'
+        element={userData ? <Search /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/editprofile'
+        element={userData ? <EditProfile /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/messages'
+        element={userData ? <Messages /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/messageArea'
+        element={userData ? <MessageArea /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/notifications'
+        element={userData ? <Notifications /> : <Navigate to={"/signin"} />}
+      />
+
+      <Route
+        path='/loops'
+        element={userData ? <Loops /> : <Navigate to={"/signin"} />}
+      />
+
     </Routes>
   )
 }
