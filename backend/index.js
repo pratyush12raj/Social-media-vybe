6438b8b53759e8340b5b1ccae6dc3c2dbefb75fb
Pratyush Raj <143428765+pratyush12raj@users.noreply.github.com>
2026-05-19 19:49:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port=process.env.PORT || 5000
 app.use(cors({
     origin:[
     "http://localhost:5173",
-    "https://vybe-frontend-3spn.onrender.com",
+     "https://vybe-5xnf.onrender.com",
         ],
     credentials:true
 }))
