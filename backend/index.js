a7b33f33614e362c85c2bb49d0454d802f4a2b18
Pratyush Raj <143428765+pratyush12raj@users.noreply.github.com>
2026-05-19 19:41:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ dotenv.config()
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:[
+    "http://localhost:5173",
+    "https://vybe-frontend-3spn.onrender.com",
+        ],
     credentials:true
 }))
 app.use(express.json())
